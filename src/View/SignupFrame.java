1bd829dcb4ae9f72cfb01f0851fe609310dbb375
NotTerrence <40994691+NotTerrence@users.noreply.github.com>
2019-03-24 12:32:48 +0800

    bg panels changed

--- src/View/SignupFrame.java
+++ src/View/SignupFrame.java
@@ -45,7 +45,7 @@ public class SignupFrame {
 		frame.setVisible(true);
 		
 		JPanel panel = new JPanel();
-		panel.setBackground(new Color(255, 204, 51));
+		panel.setBackground(new Color(103, 128, 159));
 		panel.setBounds(0, 0, 197, 56);
 		frame.getContentPane().add(panel);
 		panel.setLayout(null);
@@ -57,8 +57,8 @@ public class SignupFrame {
 		panel.add(lblLogin);
 		
 		JPanel panel_1 = new JPanel();
-		panel_1.setBackground(new Color(0, 51, 102));
-		panel_1.setBounds(0, 56, 197, 205);
+		panel_1.setBackground(new Color(108, 122, 137));
+		panel_1.setBounds(0, 56, 197, 154);
 		frame.getContentPane().add(panel_1);
 		panel_1.setLayout(null);
 		
